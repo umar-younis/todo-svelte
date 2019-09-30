6d2ba3bdd55086df83c396e1498669f1985b72ed
Babar Bilal <babarxm2@gmail.com>
2019-09-30 14:16:33 +0500

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# ToDo List using svelte
+
+```
+git clone https://github.com/codemonxter/todo-svelte.git
+```
+
+```
+cd todo-svelte
+npm install
+npm run dev
+```
+
+Navigate to http://localhost:5000
---
+++ src/input.svelte
@@ -0,0 +1,25 @@
+<script>
+let todoText = "";
+export let onAdd;
+let onClickAdd = (e) => {
+	onAdd(todoText);
+	todoText = "";
+	return false;
+}
+</script>
+
+<style>
+input {
+	width: 90%;
+}
+div {
+	margin-top: 15px;
+}
+</style>
+
+<div>
+	<form on:submit|preventDefault={onClickAdd}>
+		<input bind:value={todoText} type="text"/>
+		<button>Add</button>
+	</form>
+</div>
